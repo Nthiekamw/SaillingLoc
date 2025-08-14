15b957fd9d710e90e3af09489a1dec79ba98b076
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-08-14 13:31:42 +0200

    update dockerfile et docker compose

--- obj/Debug/net9.0/SaillingLoc.AssemblyInfo.cs
+++ obj/Debug/net9.0/SaillingLoc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaillingLoc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7037b7f89814f17eb035178eb95b4760774badf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f76b9ddd378f1290a50cb28df5139f000066df0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaillingLoc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaillingLoc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
